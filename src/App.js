2166ef477744597e8c5e10311c41e5ed19dcbe7d
mhlee80 <myeonghyeon.lee@gmail.com>
2019-05-03 12:54:18 +0900

    App에 Counter 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Counter from './components/c-components/Counter';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
+      <div style={ {textAlign: 'center'} }>
+        <Counter/>
       </div>
     );
   }
